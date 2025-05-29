6b3d7fe6dc4c43fb7ce322a2356b08f4c9cbbb08
潘迪峰 <pandifeng@thape.com.cn>
2025-05-29 18:27:10 +0800

    添加antd

---
+++ app/admin/layout.tsx
@@ -0,0 +1,9 @@
+import "@ant-design/v5-patch-for-react-19"
+
+interface AdminLayoutProps {
+  children: React.ReactNode
+}
+
+export default function AdminLayout({ children }: AdminLayoutProps) {
+  return <>11{children}</>
+}
